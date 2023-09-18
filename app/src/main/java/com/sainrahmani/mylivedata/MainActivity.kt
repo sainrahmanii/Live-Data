bf379aab1fd9dc66ff553a8375cf158b56462137
sainrahmanii <husainrahmani127@gmail.com>
2023-09-18 07:38:11 +0700

    first commit | Live Data

---
+++ app/src/main/java/com/sainrahmani/mylivedata/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.sainrahmani.mylivedata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.sainrahmani.mylivedata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var liveDataTimerViewModel: MainViewModel
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        liveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?>{ along ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, along)
+            activityMainBinding.timerTextview.text = newText
+        }
+        liveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
+    }
+}
